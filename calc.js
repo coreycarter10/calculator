bd4447d717557e5f3bc64a0b11754af815011279
Corey Carter <corey-carter@hotmail.com>
2021-11-09 15:56:15 -0700

    Added for loops for more numbers

--- calc.js
+++ calc.js
@@ -6,13 +6,19 @@ const reader = readline.createInterface({
 });
 
 
-
 reader.question("What would you like to calculate? ", function(input){
 	tokens = input.split(' ');
 	
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
+
+	let hasThirdValue = false;
+
+	if (num3 !== undefined) {
+		hasThirdValue = true;
+	}
 
 // this logs the symbol that has been inputted
 	console.log('mathSymbol', mathSymbol);
@@ -20,15 +26,35 @@ reader.question("What would you like to calculate? ", function(input){
 	console.log('num2', num2);
 
 	if (mathSymbol === '+') {
-		console.log(num1 + num2);
+		let result = tokens[1];
+		for (var i = 2; i < tokens.length; i++) {
+			result += tokens[i];
+		}
+		console.log(result);
 	} else if (mathSymbol === '-') {
-		console.log(num1 - num2);
+		let result = tokens[1];
+		for (var i = 2; i < tokens.length; i++) {
+			result -= tokens[i];
+		}
+		console.log(result);
 	} else if (mathSymbol === "*") {
-		console.log(num1 * num2);
+		let result = tokens[1];
+		for (var i = 2; i < tokens.length; i++) {
+			result *= tokens[i];
+		}
+		console.log(result);
 	} else if (mathSymbol === '/') {
-		console.log(num1 / num2);
+		let result = tokens[1];
+		for (var i = 2; i < tokens.length; i++) {
+			result /= tokens[i];
+		}
+		console.log(result);
 	} else if (mathSymbol === '√' || 'sqrt') {
 		console.log(Math.sqrt(num1));
+	} else if (mathSymbol === '^') {
+		console.log(num1 ** num2);
+	} else if(mathSymbol === '%') {
+		console.log(num1 % num2);
 	}
 
 	// This line closes the connection to the command line interface.
